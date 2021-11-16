7ec99963d9e6d1d80f7000a2afeda5063249d224
martin <martinamezola2000@gmail.com>
2021-11-16 15:17:03 +0100

    Aldaketak liburu edizioan (oraindik oso gutxi)

--- app/libEditatuDB.php
+++ app/libEditatuDB.php
@@ -1,3 +1,75 @@
 <?php
 
+    require 'dbkon.php'; //DBarekin konexioa egitea beharrezkoa baita
+
+    Session_start();
+
+
+
 ?>
+
+<!DOCTYPE html>
+<!-- Goikoaren bidez artxiboaren extensioa espezifikatzen dugu (Badaezpada) -->
+
+<!-- HTMLren hasiera -->
+<html lang="en">
+<!-- Honen bidez hmtl-k erabiliko duen hizkuntza esaten diogu ingelesa-->
+
+<!-- Web Orriarren aurrekaria-->
+<head>
+    <title>LIBURU DATUAK ALDATU</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<script type=“text/javascript” src="script2.js" ></script>
+
+
+<!--<div class="irudia">
+    <img src="https://media.istockphoto.com/photos/blue-book-picture-id1281955543?b=1&k=20&m=1281955543&s=170667a&w=0&h=ZmwacrjQewEU3RqJLYufA-Bi7JVOI2JgcB8X0o7vPeI=" alt="" width="300" height="200">
+</div>
+
+<div class="content">-->
+
+
+
+    <!-- Web Orriaren gorputza-->
+<body background="https://cdn.pixabay.com/photo/2016/02/16/21/07/books-1204029__340.jpg?__cf_chl_jschl_tk__=pmd_rg0UyIVTKotZFzKXG6L7RTRiwJwdw6vwz3E1204eRgg-1635866096-0-gqNtZGzNAjujcnBszQhR">
+    <div class="inputak">
+        <table>
+            <tr>
+                <td>&nbsp;</td>
+                <td><p style="background-color: lightblue"><strong> LIBURUTEGIA </strong></p></td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td><p style="background-color: lightblue"><strong> <Label>LIBURUEN EDIZIOA</Label> </strong></p></td>
+                <td>&nbsp;</td>
+            </tr>
+<!-- ***********************************************************************************************************-->
+
+            <tr>
+                <td>&nbsp;</td>
+                <td><p style="background-color: lightblue"><strong> Zein liburu editatu nahi duzu? </strong></p></td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr> 
+            <tr>
+                <form name="editatuAukerak" class="inputak" action="libDatuakAldatu.php" method="POST">
+                <!-- TODO -->
+                </form>
+            </tr>
+            <tr>
+                <td><input id="bueltatuBot" type="button" name="bueltatuBot" value="Bueltatu" onclick="location.href='erabileremu.php'"/></td>
+                <td>&nbsp;</td>
+            </tr>
+
+        </table>
+    </div>
+</body>
+<!-- Html dokumentuaren amaiera -->
+</html>
